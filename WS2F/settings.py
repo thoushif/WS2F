b8c7ef911798a5a3779493246479cdc63cf74fb2
Thoushif Aazam Shaik <tshaik@prokarma.com>
2020-03-31 23:16:32 -0500

    carasoul; greeting; reminder

--- WS2F/settings.py
+++ WS2F/settings.py
@@ -140,5 +140,8 @@ EMAIL_HOST_PASSWORD = 'allAboutSorry'
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+LOGIN_REDIRECT_URL = '/'
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
