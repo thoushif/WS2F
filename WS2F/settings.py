802036d0ea8ed58b19f7915bfcc687d31a616f42
Thoushif Aazam Shaik <aazam555@gmail.com>
2020-07-14 13:22:13 -0500

    Update settings.py
    added mysql db from aws

--- WS2F/settings.py
+++ WS2F/settings.py
@@ -82,8 +82,12 @@ WSGI_APPLICATION = 'WS2F.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql', 
+        'NAME': 'mycleanlydb',
+        'USER': 'thoushif',
+        'PASSWORD': 'beL0vely!',
+        'HOST': 'mycleanlydb.cqy5Innwuaqp.us-east-2.rds.amazonaws.com',   # Or an IP Address that your DB is hosted on
+        'PORT': '3306',
     }
 }
 
